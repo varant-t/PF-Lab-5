8897110082925ad0d92e65a5d03424fe7aa323bb
varant-t <vv_titizian@hotmail.ca>
2020-09-04 15:20:01 -0400

    Lab 5

---
+++ Lab 5/Assets/Scripts/Patrol.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform player;
+    public GameObject[] waypoint;
+    private int randomWp;
+    public float speed;
+    public float startWaittime;
+    public float WaitTime;
+
+
+
+    public float chaseRange;
+
+
+    bool isPatrolling = false;
+
+    void Start()
+    {
+        if (chaseRange <= 0)
+        {
+            chaseRange = 8f;
+        }
+
+        WaitTime = startWaittime;
+        randomWp = Random.Range(0, waypoint.Length);
+    }
+
+    
+    void Update()
+    {
+        if (Vector3.Distance(player.position, gameObject.transform.position) < chaseRange)
+        {
+            Chase();
+        }
+        else if (!isPatrolling)
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoint[randomWp].transform.position, speed * Time.deltaTime);
+
+
+        if(Vector3.Distance(transform.position, waypoint[randomWp].transform.position) < 0.2f )
+        {
+            if(WaitTime <= 0)
+            {
+                randomWp = Random.Range(0, waypoint.Length);
+                WaitTime = startWaittime;
+            }
+            else
+            {
+                WaitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+
+
+    private void Chase()
+    {
+        isPatrolling = false;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+  
+    }
+
+
+}
---
+++ Lab 5/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The GameObject is made to bounce using the space key.
+// Also the GameOject can be moved forward/backward and left/right.
+// Add a Quad to the scene so this GameObject can collider with a floor.
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+
+    Vector3 moveDirection = Vector3.zero;
+    public CharacterController controller;
+
+    public Transform thingToLookFrom;
+
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+
+        if (speed <= 0)
+        {
+            speed = 8.0f;
+
+            Debug.Log("Speed not set on " + name + ". Defaulting to " + speed);
+        }
+
+        if (jumpSpeed <= 0)
+        {
+            jumpSpeed = 8.0f;
+
+            Debug.Log("JumpSpeed not set on " + name + ". Defaulting to " + jumpSpeed);
+        }
+
+        if (gravity <= 0)
+        {
+            gravity = 20.0f;
+
+            Debug.Log("Gravity not set on " + name + ". Defaulting to " + gravity);
+        }
+
+
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
